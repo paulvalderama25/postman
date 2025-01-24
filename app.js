b9ad740d4f07a0bcdbd6a88f36c823da63f263e2
Paul Valderama <healthcoachpv@gmail.com>
2025-01-23 17:40:39 -0800

    second commit

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const {Client}=require('pg')
+const {Client}=require('pg');
 const express = require("express");
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+app.use('/api', createProxyMiddleware({
+    target: 'http://localhost:8080/', //original url
+    changeOrigin: true,
+    //secure: false,
+    onProxyRes: function (proxyRes, req, res) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    }
+}))
 
 const con=new Client({
     host: "localhost",
@@ -15,11 +29,13 @@ const con=new Client({
 con.connect().then(() => console.log("connected"))
 
 app.post('/postData', (req, res) => {req
-    const {name, id} = req.body
+    const {name,email,id} = req.body
 
-    const insert_query='INSERT INTO postmantable (name,id) VALUES (Paul, 1)'
+    console.log(name, email, id, 'server data')
 
-    con.query(insert_query,[name,id], (err, result) => {
+    const insert_query='INSERT INTO customers (name,email,id) VALUES ($1,$2,$3)'
+
+    con.query(insert_query,[name,email,id], (err, result) => {
         if(err){
             res.send(err)
         } else {
@@ -31,9 +47,48 @@ app.post('/postData', (req, res) => {req
 })
 
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/fetchData", (req, res) => {
+
+    const fetch_query = 'SELECT * FROM public.customers'
+
+    con.query(fetch_query, (err, result)=> {
+        if(err){
+            res.send(err)
+        } else {
+            res.send(result.rows)
+        }
+    })
 });
+
+app.put('/update/:id', (req, res) => {
+    const id = req.params.id;
+    const name = req.body.name;
+    const email = req.body.email;
+    // const {name,email,id} = req.body
+
+    const update_query = "UPDATE customers SET name=$1,email=$2 WHERE id=$3"
+
+    con.query(update_query,[name, email, id],(err,result)=>{
+        if(err){
+            console.log(err, 'whats results')
+            res.send(err)
+        }else{
+            res.send("Updated")
+        }
+    })
+})
+
+app.delete('/delete/:id', (req,res)=>{
+    const id = req.params.id;
+    const delete_query = "DELETE from customers WHERE id = $1"
+    con.query(delete_query,[id], (err, result) => {
+        if(err){
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    })
+})
 
 const PORT = 8080;
 
--- postman/src/App.js
+++ postman/src/App.js
@@ -1,17 +1,233 @@
 import './App.css';
+import {useState, useEffect} from "react";
+import axios from 'axios';
 
 function App() {
-  return (
+
+    const [customers, setCustomers] = useState([]);
+    const [search, setSearch] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [displaySearh, setDisplaySearch] = useState([]);
+    const [searchSubmitted, setSearchSubmitted] = useState(false);
+    const [noResultFound, setNoResultFound] = useState(false);
+    // const [resultFound, setResultFound] = useState(false);
+    const [editingCustomer, setEditingCustomer] = useState(false);
+    const [editedCustomerDetails, setEditedCustomerDetails] = useState({})
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [id, setId] = useState(0);
+    const [newName, setNewName] = useState('');
+    const [newEmail, setNewEmail] = useState('');
+    const [newId, setNewId] = useState(null);
+
+
+    const handleNewCustomer = (e) => {
+        e.preventDefault();
+
+        axios({
+            method: 'post',
+            url:'http://localhost:8080/postData',
+            data:{
+                name: newName,
+                email: newEmail,
+                id: newId
+            }
+        })
+        getCustomers();
+        setNewName('');
+        setNewId(0);
+        setNewEmail('');
+    }
+    const editCustomer = (id, name, email) => {
+        setEditingCustomer(true);
+        setNoResultFound(false);
+        setSearchSubmitted(false);
+        setName(name);
+        setEmail(email);
+        setId(id);
+        // setEditedCustomerDetails({
+        //     "name": name,
+        //     "email": email,
+        //     "id": id
+        // })
+    }
+
+    const deleteCustomer = (e, id) => {
+        e.preventDefault();
+
+        axios.delete('http://localhost:8080/delete/' + id)
+            .then((res, err) => {
+                console.log(res)
+        })
+            .catch((error) => {
+            console.log(error)
+        })
+
+        setNoResultFound(false);
+        setSearchSubmitted(false);
+        setEditingCustomer(false);
+        getCustomers();
+        setSearch('');
+    }
+
+    const handleEditSubmit = (e) => {
+        e.preventDefault();
+        axios.put('http://localhost:8080/update/' + id, {name, email})
+            .then((res, err) => {
+                if(res.data) {
+                    console.log('updated')
+                } else {
+                    console.log(err, 'customer not updated')
+                }
+            })
+        setEditingCustomer(false);
+        setNoResultFound(false);
+        setSearchSubmitted(false);
+        setSearch('');
+    }
+
+    const submitSearch = () => {
+        setSearchSubmitted(true);
+        setSearch('');
+        setEditingCustomer(false);
+    }
+
+    const displayAllCustomers = () => {
+        setSearchSubmitted(false);
+        setSearchResults([]);
+        setNoResultFound(false);
+    }
+    function getCustomers() {
+        fetch('/fetchData')
+            .then(response => response.json())
+            // .then(data => console.log(data))
+            .then(data => {
+                console.log(data)
+                setCustomers(data)
+            })
+            .catch(error => console.error(error))
+    }
+
+    const handleChange = (event) => {
+
+        setSearch(event.target.value);
+        const filterCustomers = customers.filter((item) => {
+            if (item.id === Number(event.target.value)){
+                // setResultFound(true);
+                console.log(item, 'item in handle')
+                return item;
+            }
+        });
+        if(filterCustomers.length > 0){
+            console.log(filterCustomers, 'hitting filter')
+            setSearchResults(filterCustomers[0]);
+            setNoResultFound(false);
+            setSearchSubmitted(true);
+        } else {
+            setSearchResults([]);
+            setSearchSubmitted(false);
+            setNoResultFound(true);
+        }
+        console.log(searchSubmitted,'search sub')
+    }
+
+    useEffect(() => {
+        getCustomers();
+    }, [editingCustomer, noResultFound]);
+
+    // useEffect(() => {
+    //     setDisplaySearch(searchResults)
+    // }, [searchResults])
+
+    return (
     <div className="App">
-        <p>Input Postman Collection ID</p>
+        <p>Search for Customer by ID</p>
         <div>
-          <input/>
-          <button>Test</button>
+          <input
+            type='text'
+            value={search}
+            onChange={handleChange}
+          />
+          <button
+            onClick={submitSearch}
+          >Submit</button>
+            <button
+                onClick={displayAllCustomers}
+            >Display All Customers
+            </button>
         </div>
-          <div>
-              <p className="current-results">Current Validation Results</p>
-              <p className="previous-results">Previous Validation Results</p>
-          </div>
+        <div>
+            <p>Enter New Customer</p>
+            Name
+            <input
+                value={newName}
+                onChange={(e) => {setNewName(e.target.value)}}
+            />
+            Email
+            <input
+                value={newEmail}
+                onChange={(e) => {setNewEmail(e.target.value)}}
+            />
+            ID
+            <input
+                value={newId}
+                onChange={(e) => {setNewId(e.target.value)}}
+                placeholder={''}
+            />
+            <button
+                onClick={(e) => {handleNewCustomer(e)}}
+            >Submit New Customer</button>
+        </div>
+        <div>
+            {searchSubmitted ?
+                <p className="customer-title">Searched Customer</p>
+                :
+                <p className="customer-title">Customers</p>
+            }
+        </div>
+        <div className='result-list'>
+            { searchSubmitted && !noResultFound ?
+                // { searchSubmitted && !noResultFound && !editingCustomer ?
+                <ul>
+                    <li key={"name"}>Name: {searchResults.name}</li>
+                    <li key={'email'}>Email: {searchResults.email}</li>
+                    <li key={'id'}>ID: {searchResults.id}</li>
+                    <button
+                        onClick={() => {editCustomer(searchResults.id, searchResults.name, searchResults.email)}}
+                    >edit
+                    </button>
+                    <button
+                        onClick={(e) => {deleteCustomer(e, searchResults.id)}}
+                    >delete</button>
+                </ul>
+
+                : noResultFound && searchSubmitted && !editingCustomer ?
+                    <p>No Result Found</p>
+                    : !searchSubmitted && !noResultFound &&  editingCustomer?
+                        <span>
+                            Name:
+                            <input
+                                value={name}
+                                onChange={e => setName(e.target.value)}
+                            />
+                            Email:
+                            <input
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                            <button
+                                onClick={handleEditSubmit}
+                            >Submit Edit</button>
+                        </span>
+                : customers.map((item, index) => (
+                    <ul>
+                        <li key={"name"}>Name: {item.name}</li>
+                        <li key={'email'}>Email: {item.email}</li>
+                        <li key={'id'}>ID: {item.id}</li>
+                    </ul>
+                ))
+            }
+        </div>
     </div>
   );
 }
