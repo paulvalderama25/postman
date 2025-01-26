074ca9cc6e13c516267dbcc9cc68539dbe5a7577
Paul Valderama <healthcoachpv@gmail.com>
2025-01-25 18:50:30 -0800

    node fix

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ app.use(cors());
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.use('/api', createProxyMiddleware({
     // target: 'http://localhost:8080/', //original url
